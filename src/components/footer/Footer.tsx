0625692ead8fb6bb7a42fd988034ef4597aa7cd0
VictorOPerez <ortegaviktor571@gmail.com>
2023-11-25 08:44:57 -0500

    first commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>VM-admin</span>
+      <span>VM Admin Dashboart</span>
+    </div>
+  );
+};
+
+export default Footer;
